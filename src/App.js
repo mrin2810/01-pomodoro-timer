f2f2a4cf8ad7023e05b9120a0078c1362e685d84
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-10 18:28:19 +0530

    Converted the timeLeft var to minutes and seconds

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [timeLeft, setTimeLeft] = useState(25*60)
+
+  const [timeLeft, setTimeLeft] = useState(25*60);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = (timeLeft - (minutes*60));
+
   return (
     <div className="app">
       <h2>Pomodoro!</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
