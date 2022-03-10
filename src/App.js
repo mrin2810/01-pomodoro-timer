1fe9e290cd710ccb5d2a27d1c4c6e1e9488b59d1
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-10 18:27:13 +0530

    Added state to count the time.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [timeLeft, setTimeLeft] = useState(25*60)
   return (
     <div className="app">
       <h2>Pomodoro!</h2>
